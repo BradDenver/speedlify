f97b68eaa3bbd6e50ee6358cbde14543880c9a5d
BradDenver <brad.denver@gmail.com>
2020-08-14 15:00:36 +1000

    Initial commit

---
+++ _data/sites/staxio.js
@@ -0,0 +1,23 @@
+module.exports = {
+	name: "stax.io", // optional, falls back to object key
+	description: "Stax public web site",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify (not Zach’s)
+	// skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	options: {
+		frequency: 60 * 1, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site",
+    // Data directory, results go into `_data/manual` instead of `_data/results`
+		useManualResultsDir: true,
+	},
+	urls: [
+		"https://www.stax.io/",
+		"https://www.stax.io/careers/",
+		"https://www.stax.io/docs/",
+		"https://www.stax.io/blog/2020-02-26-why-we-built-stax-frombespoke-buildsto-automated-best-practice-deployments/",
+	]
+};
